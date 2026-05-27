66419a6dd8c9cd43903d249b85bdfc08f90f69a9
Sathya1792 <50834589+Sathya1792@users.noreply.github.com>
2026-05-27 14:07:29 +0530

    Add files via upload

---
+++ week1_oddoreven.js
@@ -0,0 +1,14 @@
+ let number;
+function isoddoreven(number)
+{
+   
+    if (number%2===0){
+        return ("Number is even")
+    }
+    else{
+        return("number is Odd")
+    }
+}
+
+console.log(isoddoreven(4))
+
