9fd2572f864a3d723e40c33a2a5ddaa60f15f3f4
Sathya1792 <50834589+Sathya1792@users.noreply.github.com>
2026-05-27 14:08:54 +0530

    Add files via upload

---
+++ week1numtype.js
@@ -0,0 +1,14 @@
+let number = 3;
+function numtype(number) {
+    if (number > 0) {
+        return ("positive");
+    } else if (number < 0) {
+        return ("negative");
+    } else {
+        return ("neutral");
+   
+    }
+}
+
+console.log(numtype(number));
+
